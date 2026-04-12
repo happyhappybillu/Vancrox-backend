778e48bcb7d0316cabcc981340c96df521e4eddb
happyhappybillu <happyhappybilluu@gmail.com>
2026-04-12 11:05:15 +0530

    newmkkn

--- utils/uidTid.js
+++ utils/uidTid.js
@@ -2,12 +2,13 @@ const User = require("../models/User");
 
 async function generateUID() {
   const last = await User.findOne({ role: "investor", uid: { $gt: 0 } }).sort({ uid: -1 }).lean();
-  return last?.uid ? last.uid + 1 : 103500;
+  const nextUid = last?.uid ? last.uid + 1 : 103500;
+  return nextUid < 103500 ? 103500 : nextUid;
 }
 
 async function generateTID() {
   const last = await User.findOne({ role: "trader", tid: { $gt: 0 } }).sort({ tid: -1 }).lean();
-  return last?.tid ? last.tid + 1 : 50001;
+  return last?.tid ? last.tid + 1 : 50555;
 }
 
 module.exports = { generateUID, generateTID };
